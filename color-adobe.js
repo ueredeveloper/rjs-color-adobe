fa5f6a5e29b4ac8ab79d8602c12ca02ad37279f1
ueredeveloper <ueredeveloper@gmail.com>
2023-02-27 23:26:05 -0300

    first commit

---
+++ color-adobe.js
@@ -0,0 +1,364 @@
+const adobeColors = [
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/d9b5bc121546395.60c93841a97b0.jpg",
+        "colors": [
+            "rgb(242, 98, 46)",
+            "rgb(242, 48, 5)",
+            "rgb(242, 153, 133)",
+            "rgb(242, 53, 53)",
+            "rgb(242, 242, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/b01d30162258123.Y3JvcCwzMTkzLDI0OTcsMCw3MDM.jpg",
+        "colors": [
+            "rgb(140, 3, 53)",
+            "rgb(10, 38, 25)",
+            "rgb(90, 191, 134)",
+            "rgb(166, 162, 143)",
+            "rgb(191, 151, 128)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/22e7e2161428409.Y3JvcCwxNTk5LDEyNTAsMCwzNjA.jpg",
+        "colors": [
+            "rgb(217, 13, 145)",
+            "rgb(242, 5, 179)",
+            "rgb(217, 167, 74)",
+            "rgb(166, 104, 50)",
+            "rgb(140, 69, 53)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/35cce1160448521.Y3JvcCwxMTIwLDg3NiwwLDI2MA.jpg",
+        "colors": [
+            "rgb(1, 35, 64)",
+            "rgb(1, 21, 38)",
+            "rgb(3, 103, 166)",
+            "rgb(3, 88, 140)",
+            "rgb(191, 151, 128)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/736960155616829.Y3JvcCwzOTU0LDMwOTMsMCwyMzU5.jpg",
+        "colors": [
+            "rgb(140, 15, 26)",
+            "rgb(0, 8, 13)",
+            "rgb(140, 124, 109)",
+            "rgb(191, 124, 99)",
+            "rgb(242, 181, 167)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/0bf50580527479.Y3JvcCw5NTMsNzQ1LDIwNSw2NDE.jpg",
+        "colors": [
+            "rgb(93, 90, 191)",
+            "rgb(4, 15, 217)",
+            "rgb(3, 28, 166)",
+            "rgb(5, 199, 242)",
+            "rgb(3, 166, 44)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/6ce42a157966519.Y3JvcCwxNTMzLDExOTksMCwxNDA.jpg",
+        "colors": [
+            "rgb(3, 103, 166)",
+            "rgb(1, 64, 35)",
+            "rgb(217, 204, 193)",
+            "rgb(217, 144, 102)",
+            "rgb(89, 9, 2)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/9acaf195326977.Y3JvcCw4MTYsNjM4LDI5MCw1NDU.jpg",
+        "colors": [
+            "rgb(242, 214, 189)",
+            "rgb(166, 101, 78)",
+            "rgb(217, 163, 143)",
+            "rgb(242, 48, 5)",
+            "rgb(89, 50, 46)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/cdbc9f162900397.Y3JvcCwyMjUwLDE3NTksMCww.jpg",
+        "colors": [
+            "rgb(28, 40, 64)",
+            "rgb(23, 28, 38)",
+            "rgb(101, 147, 166)",
+            "rgb(176, 209, 217)",
+            "rgb(206, 236, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/72254e160352365.Y3JvcCwxNjY1LDEzMDIsNDQxLDc1MA.jpg",
+        "colors": [
+            "rgb(126, 140, 73)",
+            "rgb(191, 111, 65)",
+            "rgb(191, 99, 65)",
+            "rgb(140, 52, 32)",
+            "rgb(38, 18, 18)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/3dd3e5162242353.Y3JvcCwyMjg1LDE3ODcsNzI0LDEzMDY.jpg",
+        "colors": [
+            "rgb(242, 242, 242)",
+            "rgb(191, 191, 191)",
+            "rgb(115, 115, 115)",
+            "rgb(64, 64, 64)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/25d5ee163493667.63e67ebe699ba.jpg",
+        "colors": [
+            "rgb(242, 237, 220)",
+            "rgb(242, 182, 148)",
+            "rgb(217, 137, 108)",
+            "rgb(140, 118, 114)",
+            "rgb(64, 1, 1)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/1bd4ba164163449.Y3JvcCwxMzExLDEwMjUsODU2LDI4OA.jpg",
+        "colors": [
+            "rgb(166, 3, 47)",
+            "rgb(242, 61, 109)",
+            "rgb(64, 1, 1)",
+            "rgb(38, 1, 1)",
+            "rgb(242, 242, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/7e9a9f163849009.Y3JvcCwxNjAwLDEyNTEsMCw1OTg.jpg",
+        "colors": [
+            "rgb(242, 92, 190)",
+            "rgb(17, 48, 140)",
+            "rgb(19, 116, 242)",
+            "rgb(19, 138, 242)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/401425163350535.Y3JvcCwyNDAwLDE4NzcsMCw1NTg.jpg",
+        "colors": [
+            "rgb(64, 6, 16)",
+            "rgb(191, 38, 66)",
+            "rgb(217, 189, 106)",
+            "rgb(217, 165, 154)",
+            "rgb(166, 102, 96)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/9bc0d4162646851.Y3JvcCwzODQwLDMwMDMsMCw5Njk.jpg",
+        "colors": [
+            "rgb(242, 228, 216)",
+            "rgb(166, 115, 86)",
+            "rgb(217, 189, 173)",
+            "rgb(89, 55, 47)",
+            "rgb(38, 23, 23)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/8d1297163133193.Y3JvcCwxNTgyLDEyMzcsMCw1NzE.jpg",
+        "colors": [
+            "rgb(242, 242, 242)",
+            "rgb(217, 217, 217)",
+            "rgb(140, 140, 140)",
+            "rgb(38, 38, 38)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/a023b6159738079.63a479d95caff.jpg",
+        "colors": [
+            "rgb(37, 38, 32)",
+            "rgb(242, 242, 242)",
+            "rgb(217, 216, 215)",
+            "rgb(166, 159, 151)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/bff9ef161005417.Y3JvcCwxMzM0LDEwNDMsNTYsNjI.jpg",
+        "colors": [
+            "rgb(113, 147, 217)",
+            "rgb(140, 52, 20)",
+            "rgb(89, 18, 2)",
+            "rgb(38, 1, 1)",
+            "rgb(89, 67, 67)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/43f908163368401.Y3JvcCwxNDM1LDExMjMsMCwyMzQ.jpg",
+        "colors": [
+            "rgb(191, 139, 42)",
+            "rgb(242, 204, 182)",
+            "rgb(166, 102, 88)",
+            "rgb(217, 59, 59)",
+            "rgb(242, 242, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/df6312163413309.Y3JvcCwzNTIzLDI3NTUsMCwzNA.jpg",
+        "colors": [
+            "rgb(242, 179, 214)",
+            "rgb(241, 172, 242)",
+            "rgb(46, 2, 89)",
+            "rgb(39, 3, 166)",
+            "rgb(68, 19, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/d18be4156687963.Y3JvcCw1OTc2LDQ2NzQsNDg4LDE3NzU.jpg",
+        "colors": [
+            "rgb(109, 137, 140)",
+            "rgb(242, 215, 133)",
+            "rgb(217, 164, 65)",
+            "rgb(166, 79, 3)",
+            "rgb(64, 6, 1)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/111833163255499.Y3JvcCwyNjQ2LDIwNjksMCww.jpg",
+        "colors": [
+            "rgb(242, 5, 135)",
+            "rgb(242, 153, 202)",
+            "rgb(242, 5, 159)",
+            "rgb(217, 217, 217)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/ecc3de153101245.Y3JvcCwxNTM0LDEyMDAsMzQsMA.jpg",
+        "colors": [
+            "rgb(89, 68, 50)",
+            "rgb(242, 205, 172)",
+            "rgb(64, 28, 7)",
+            "rgb(217, 158, 137)",
+            "rgb(115, 93, 85)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/b2c6cb161358593.Y3JvcCwxODMyLDE0MzIsMCw3ODk.jpg",
+        "colors": [
+            "rgb(115, 36, 42)",
+            "rgb(208, 213, 217)",
+            "rgb(166, 122, 106)",
+            "rgb(140, 90, 79)",
+            "rgb(38, 1, 1)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/023289160506507.Y3JvcCw5OTksNzgyLDAsNjE.jpeg",
+        "colors": [
+            "rgb(242, 242, 242)",
+            "rgb(166, 166, 166)",
+            "rgb(115, 115, 115)",
+            "rgb(64, 64, 64)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/b9f2e9163214959.Y3JvcCwxNDAwLDEwOTUsMCw0NTc.jpg",
+        "colors": [
+            "rgb(81, 50, 89)",
+            "rgb(78, 89, 54)",
+            "rgb(115, 57, 44)",
+            "rgb(166, 117, 106)",
+            "rgb(64, 15, 10)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/d32af4144033065.62875b2148f75.jpg",
+        "colors": [
+            "rgb(191, 104, 54)",
+            "rgb(191, 143, 115)",
+            "rgb(166, 88, 60)",
+            "rgb(89, 40, 29)",
+            "rgb(242, 242, 242)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/d80b3e162299091.Y3JvcCwxMzMzLDEwNDIsMCw1MTg.jpg",
+        "colors": [
+            "rgb(190, 107, 191)",
+            "rgb(122, 79, 140)",
+            "rgb(13, 17, 38)",
+            "rgb(77, 104, 140)",
+            "rgb(115, 156, 191)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/a178ea160351765.Y3JvcCwxNjMyLDEyNzYsMCw2OTg.jpg",
+        "colors": [
+            "rgb(191, 96, 113)",
+            "rgb(6, 84, 115)",
+            "rgb(191, 162, 107)",
+            "rgb(166, 123, 119)",
+            "rgb(89, 46, 46)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/de60b8139812729.Y3JvcCwxMzM0LDEwNDMsMCwxNzE.jpg",
+        "colors": [
+            "rgb(191, 23, 37)",
+            "rgb(2, 94, 115)",
+            "rgb(109, 166, 129)",
+            "rgb(191, 141, 122)",
+            "rgb(64, 1, 1)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/89e01f160678813.63b98361ef61e.jpg",
+        "colors": [
+            "rgb(42, 51, 64)",
+            "rgb(115, 85, 60)",
+            "rgb(166, 131, 101)",
+            "rgb(140, 110, 93)",
+            "rgb(64, 18, 1)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/8b8ded159049111.Y3JvcCw1NDkzLDQyOTYsMCwxNDU0.jpg",
+        "colors": [
+            "rgb(242, 242, 242)",
+            "rgb(217, 217, 217)",
+            "rgb(115, 115, 115)",
+            "rgb(64, 64, 64)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/7ff0d4150160205.Y3JvcCwyODQ3LDIyMjcsMCwxMDIy.jpg",
+        "colors": [
+            "rgb(242, 242, 242)",
+            "rgb(166, 166, 166)",
+            "rgb(89, 89, 89)",
+            "rgb(38, 38, 38)",
+            "rgb(13, 13, 13)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/b1c16f160447699.Y3JvcCwxMTc4LDkyMSwwLDI3MQ.jpg",
+        "colors": [
+            "rgb(191, 4, 73)",
+            "rgb(217, 11, 107)",
+            "rgb(16, 38, 19)",
+            "rgb(31, 64, 33)",
+            "rgb(191, 44, 31)"
+        ]
+    },
+    {
+        "img": "https://mir-s3-cdn-cf.behance.net/projects/202/c50b33154030015.Y3JvcCwxMjE5LDk1Myw3OCwyMjk.jpg",
+        "colors": [
+            "rgb(169, 171, 217)",
+            "rgb(194, 201, 242)",
+            "rgb(2, 31, 89)",
+            "rgb(115, 68, 41)",
+            "rgb(191, 121, 96)"
+        ]
+    }
+]
+
+export {adobeColors}
